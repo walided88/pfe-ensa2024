b1edd3c94be358858b657c2cb06b76952db172ac
walided88 <walideddaif@gmail.com>
2024-07-14 16:31:16 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './store/store';
 import { openDatabase, createTable } from './services/Database';
 import Orientation from 'react-native-orientation-locker';
 import { StatusBar } from 'react-native';
 import './i18n';
 import HomeScreen from './screens/HomeScreen';
 import GameScreen from './screens/GameScreen';
+import Game3Screen from './screens/Game3Screen';
+import Game4Screen from './screens/Game4Screen';
+
 import Game2Screen from './screens/Game2Screen';
 import ResultsScreen from './screens/ResultsScreen';
-import ComponentListScreen from './screens/ComponentListScreen';
+import ListScreen from './screens/ListScreen';
 import GeminiHelper from './components/GeminiHelper';
-import GameView from './view/GameView'; // Assurez-vous que le chemin est correct
-import { SafeAreaView, Text, Button, View } from 'react-native';
-import { useTranslation } from 'react-i18next';
+import GameView from './screens/GameView'; // Assurez-vous que le chemin est correct
+import { Provider } from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
@@ -38,7 +39,7 @@ const App = () => {
       Orientation.unlockAllOrientations();
     };
   }, []);
-  return (
+  return ( 
     <Provider store={store}>
           
       <NavigationContainer>
@@ -46,8 +47,11 @@ const App = () => {
 
 
           <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="ComponentList" component={ComponentListScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="ComponentList" component={ListScreen} options={{ headerShown: false }} />
           <Stack.Screen name="Game" component={GameScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Game4" component={Game4Screen} options={{ headerShown: false }} />
+
+          <Stack.Screen name="Game3" component={Game3Screen} options={{ headerShown: false }} />
           <Stack.Screen name="Game2" component={Game2Screen} options={{ headerShown: false }} />
           <Stack.Screen name="Gemini" component={GeminiHelper} options={{ headerShown: false }} />
           <Stack.Screen name="GameView" component={GameView} options={{ headerShown: false }} />
