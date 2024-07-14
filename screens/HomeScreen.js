21580c62a927b5b7cc5f91e68343d2db743f582f
walided88 <walideddaif@gmail.com>
2024-07-14 15:38:11 +0100

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,39 +1,43 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Animated} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { addPlayer, getPlayers, checkPlayerExists } from '../services/Database';
-import { setPlayerId } from '../redux/actions';
+import { addPlayer, getPlayers, checkPlayerExists,getPlayerByName } from '../services/Database';
+import { setPlayerId } from '../store/actions';
 import BgMusique from "../components/BgMusique";
-import CollisionHandler from '../systems/CollisionHandler';
 import FastImage from 'react-native-fast-image';
 import { useTranslation } from 'react-i18next';
 import { SafeAreaView, Picker } from '@react-native-picker/picker';
-import '../i18n';
-import 'intl';
-import 'intl/locale-data/jsonp/en'; // Ajoutez d'autres langues si nécessaire
-import store from '../redux/store';
-import { Provider } from 'react-redux';
+
+/**
+ * Composant HomeScreen
+ * 
+ * Le composant HomeScreen est l'écran d'accueil de l'application où les utilisateurs peuvent entrer leur nom et sélectionner une langue.
+ * Il comporte des éléments animés et s'intègre avec un store Redux pour gérer les données des joueurs.
+ */
 
 const HomeScreen = React.memo(({ navigation }) => {
+  // État pour stocker la liste des joueurs
   const [players, setPlayers] = useState([]);
+  // État pour stocker le nom entré
   const [name, setName] = useState('');
+  // État pour stocker le message de bienvenue pour l'utilisateur
   const [welcomeMessage, setWelcomeMessage] = useState('');
+  // Hook de dispatch Redux
   const dispatch = useDispatch();
-  const playerId = useSelector((state) => state.playerId);
+  // Hook de traduction
   const { t, i18n } = useTranslation();
 
+  // Référence pour gérer la valeur de l'animation
   const animatedValue = useRef(new Animated.Value(1)).current;
+  // Référence pour gérer l'opacité du message de bienvenue
   const welcomeOpacity = useRef(new Animated.Value(0)).current;
 
+  // Fonction pour changer la langue avec i18n
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
-  const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.languageButton} onPress={() => changeLanguage(item.value)}>
-      <Text style={styles.languageText}>{item.label}</Text>
-    </TouchableOpacity>
-  );
-  CollisionHandler
+
+  // Hook pour récupérer la liste des joueurs et démarrer la boucle d'animation lors du montage du composant
   useEffect(() => {
     getPlayers(setPlayers);
 
@@ -53,22 +57,43 @@ const HomeScreen = React.memo(({ navigation }) => {
     ).start();
   }, [animatedValue]);
 
+  // Fonction asynchrone pour gérer l'ajout d'un joueur et la navigation vers l'écran suivant
   const handleAddPlayerAndNavigate = useCallback(async () => {
     if (name) {
+      
       try {
-        const exists = await checkPlayerExists(name);
-        if (exists) {
-          setWelcomeMessage(`Welcome back, ${name}!`);
-        } else {
-          addPlayer(name, (playerId) => {
-            dispatch(setPlayerId(playerId));
-            setWelcomeMessage(t('welcome') );
-          });
-        }
 
-        getPlayers(setPlayers);
+
+
+
+
+        checkPlayerExists(name)
+        .then(({ exists, playerId }) => {
+          if (exists) {
+            setWelcomeMessage(`${t('welcome back')}, ${name}!`);
+            dispatch(setPlayerId(playerId));
+
+            console.log(`Player found with ID: ${playerId}`);
+            // Continuez avec la logique de votre application
+          } else {
+            addPlayer(name, (playerId) => {
+              dispatch(setPlayerId(playerId));
+              setWelcomeMessage(`${t('welcome')}, ${name}!`);
+            });
+
+            console.log('Player not found',playerId);
+          }
+          dispatch(setPlayerId(playerId));
+
+        })
+        .catch(error => {
+          console.log('Error checking player existence:', error);
+        });
+      
+
         setName('');
 
+        // Animation pour afficher puis masquer le message de bienvenue
         Animated.timing(welcomeOpacity, {
           toValue: 1,
           duration: 500,
@@ -86,41 +111,32 @@ const HomeScreen = React.memo(({ navigation }) => {
           }, 2000);
         });
       } catch (error) {
-        console.error("Error checking player existence or adding player:", error);
+        console.error("Erreur lors de la vérification de l'existence du joueur ou de l'ajout du joueur:", error);
       }
     }
   }, [name, dispatch, welcomeOpacity, navigation]);
 
   return (
-
-    
     <FastImage
-    source={require('../assets/images/math.gif')}
-    style={styles.background}
-    resizeMode={FastImage.resizeMode.cover}
-  >
-
-      <Provider store={store}>
-
-<View style={{ flexDirection: 'row', marginTop: 20 }}>
-          <Picker
-            selectedValue={i18n.language}
-            style={{ height: 50, width: 150 ,    backgroundColor: 'orange',
-            }}
-            onValueChange={(itemValue) => changeLanguage(itemValue)}
-          >
-            <Picker.Item label="English" value="en" />
-            <Picker.Item label="Français" value="fr" />
-          </Picker>
-        </View>
-        </Provider>
+      source={require('../assets/images/math.gif')}
+      style={styles.background}
+      resizeMode={FastImage.resizeMode.cover}
+    >
+      <View style={styles.languagePickerContainer}>
+        <Picker
+          selectedValue={i18n.language}
+          style={styles.languagePicker}
+          onValueChange={(itemValue) => changeLanguage(itemValue)}
+        >
+          <Picker.Item label="English" value="en" />
+          <Picker.Item label="Français" value="fr" />
+        </Picker>
+      </View>
       <View style={styles.container}>
-      <Text>{t('welcome')}</Text>
-
-        <BgMusique musicFile="bg.mp3" volume={0.7} /> 
+        <BgMusique musicFile="bg.mp3" volume={0.7} />
         <TextInput
           style={styles.input}
-          placeholder="   Enter your name"
+          placeholder={t('Enter your name')}
           value={name}
           onChangeText={setName}
         />
@@ -138,18 +154,24 @@ const HomeScreen = React.memo(({ navigation }) => {
           </Animated.View>
         )}
       </View>
-
-      </FastImage>
-    
+    </FastImage>
   );
 });
 
-export default HomeScreen;
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
     resizeMode: 'cover',
+  },
+  languagePickerContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  languagePicker: {
+    height: 40,
+    width: '25%',
+    backgroundColor: 'orange',
   },
   container: {
     flex: 1,
@@ -177,8 +199,6 @@ const styles = StyleSheet.create({
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 5,
   },
   buttonText: {
     color: '#FFF',
@@ -204,5 +224,5 @@ const styles = StyleSheet.create({
     textShadowOffset: { width: 2, height: 2 },
     textShadowRadius: 1,
   },
-  
 });
+export default HomeScreen;
